607ecd6f2e9fe4d9f2fa6bea683e952e1adc61a2
Khylko Andrii <hilko_a@itstep.academy>
2024-03-20 16:29:07 +0200

    Add button Push

--- WpfTestProject_P15/MainWindow.xaml.cs
+++ WpfTestProject_P15/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace WpfTestProject_P15
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
